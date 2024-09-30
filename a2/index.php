096ec5607c3f08888f7b1b82be0556517db1161f
Kristopher <s4007026@student.rmit.edu.au>
2024-09-30 23:16:51 +1000

    yes

--- a2/index.php
+++ a2/index.php
@@ -3,5 +3,5 @@
 include('includes/db_connect.inc'); // Database connection
 include('includes/header.inc'); // Header content
 include('includes/nav.inc'); // Navigation content
-include('C:\xampp\htdocs\wp\a1\index.html'); // or require 'index.html';
+include('C:\xampp\htdocs\wp\a1\index.html');
 ?>
